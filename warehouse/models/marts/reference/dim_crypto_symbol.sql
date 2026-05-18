b65ba6fd67b3901eac0425c28f8942167c80d559
Soheil Ebrahimi <ebrahimisoheil72@gmail.com>
2026-05-18 23:31:23 +0200

    feat: add dim_crypto_symbol reference mart

---
+++ warehouse/models/marts/reference/dim_crypto_symbol.sql
@@ -0,0 +1,23 @@
+WITH snapshot AS (
+    SELECT *
+    FROM {{ ref('snap_dim_crypto_symbol') }}
+    WHERE dbt_valid_to IS NULL
+),
+
+universe AS (
+    SELECT symbol, coingecko_id
+    FROM {{ ref('crypto_universe') }}
+)
+
+SELECT
+    s.coin_id,
+    s.symbol,
+    s.name,
+    s.market_rank,
+    s.market_cap,
+    s.category,
+    s.chain,
+    u.coingecko_id,
+    s.fetched_at
+FROM snapshot AS s
+LEFT JOIN universe AS u ON s.coin_id = u.coingecko_id
